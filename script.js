7c9865dd9316f29aa9d68fcd5652c5b4e432bdcd
evgenyfokin <fokin_e@inbox.ru>
2022-06-04 11:23:39 +0700

    done

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-'use strict'
+'use strict';
+
+const messageInput = document.getElementById('message-input');
+
+function getMessage() {
+    console.log(messageInput.value);
+    document.getElementById('message-output').innerHTML = messageInput.value;
+    messageInput.value = '';
+}
