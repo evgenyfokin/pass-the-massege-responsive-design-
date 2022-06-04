9fd4923df7f6b458904287d8631cf2febd6a01a9
evgenyfokin <fokin_e@inbox.ru>
2022-06-04 22:02:18 +0700

    Added passing by 'enter key'

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
 
 const messageInput = document.getElementById('message-input');
 
+messageInput.addEventListener('keydown', function(event){
+    if(event.key == 'Enter')
+    getMessage();
+})
+
 function getMessage() {
     console.log(messageInput.value);
     document.getElementById('message-output').innerHTML = messageInput.value;
